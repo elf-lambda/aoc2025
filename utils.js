6d74f0acae89f129074bfad6d33fff2467e4ade2
elf-lambda <167811694+elf-lambda@users.noreply.github.com>
2025-12-05 19:45:15 +0200

    updated utils

--- utils.js
+++ utils.js
@@ -3,7 +3,25 @@ import { readFileSync } from "fs";
 export const getInputLines = () => {
     const input = readFileSync(process.argv[2] || "input.txt", "utf8");
     // \r\n for windows
-    return input.trim().split("\r\n");
+    return input.trim().replaceAll("\r", "").split("\n");
+};
+
+export const getInputLinesByComma = () => {
+    const input = readFileSync(process.argv[2] || "input.txt", "utf8");
+    return input.replace("\r\n", "").split(",");
+};
+
+export const getCustomInputLines = () => {
+    const input = readFileSync(
+        process.argv[2] || "input.txt",
+        "utf8"
+    ).replaceAll("\r", "");
+    const sections = input.trim().split("\n\n");
+
+    const firstList = sections[0].split("\n");
+    const secondList = sections[1].split("\n");
+
+    return [firstList, secondList];
 };
 
 export const print = console.log;
